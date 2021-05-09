915c9978f949ca31c560121a8e4cfed9ed1a46d6
Kobe <ingo.stoecker@gmail.com>
2021-05-09 22:07:13 +0200

    show pokemon ditto data with 60sec cache

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,27 @@
+import { getDisplayName } from 'next/dist/next-server/lib/utils'
 import Head from 'next/head'
 import { Fragment } from 'react'
 
-export default function Home() {
+export async function getStaticProps() {
+  console.log(new Date())
+  const dittoResponse = await fetch('https://pokeapi.co/api/v2/pokemon/pikachu')
+
+  if (dittoResponse.status !== 200) {
+    console.error(json)
+    throw new Error('Failed to fetch API')
+  }
+
+  const dittoData = await dittoResponse.json()
+
+  return {
+    props: {
+      dittoData
+    },
+    revalidate: 60
+  }
+}
+
+export default function Home({dittoData}) {
   return (
     <Fragment>
       <Head>
@@ -13,10 +33,17 @@ export default function Home() {
         <h1>Pokedex</h1>
 
         <dl>
-          <dt>tbd</dt>
-          <dd>tbd</dd>
+          <dt>{dittoData.order}</dt>
+          <dd>{dittoData.name}</dd>
         </dl>
       </main>
+
+      <style jsx>{`
+        dl {
+          display: flex;
+        }
+      `}
+      </style>
     </Fragment>
   )
 }
